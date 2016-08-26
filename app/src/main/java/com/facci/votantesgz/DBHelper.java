2a238c89cd28a5ce2b15424a7129ccae13de86ac
GABRIEL_SZM <gabriel_szm@outlook.com>
2016-08-25 23:13:19 -0500

    Initial commit

---
+++ app/src/main/java/com/facci/votantesgz/DBHelper.java
@@ -0,0 +1,101 @@
+package com.facci.votantesgz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by GABRIEL_SZM on 25/08/2016.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NOMBRE = "CNEGZ.db";
+    public static final String TABLA_NOMBRE = "VOTANTESGZ";
+
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NOMBRE";
+    public static final String COL_3 = "APELLIDO";
+    public static final String COL_4 = "RECINTO_ELECTORAL";
+    public static final String COL_5 = "ANO_NACIMIENTO";
+
+    public DBHelper(Context context) {
+        super(context, name, factory, version);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(String.format("create table %s (ID INTEGER PRIMARY KEY AUTOINCREMENT,%s TEXT,%s TEXT,%s TEXT,%s INTEGER)",TABLA_NOMBRE,COL_2,COL_3,COL_4,COL_5));
+        Log.i("--------------------------------------------------------> Base de datos:"+DB_NOMBRE, "DB CREADA");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        db.execSQL(String.format("DROP TABLE IF EXISTS %s",TABLA_NOMBRE));
+        onCreate(db);
+    }
+    public boolean IngresarDatos(String nombre,String apellido,String recinto, String anio ){
+        SQLiteDatabase db =this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(COL_2,nombre);
+        contentValues.put(COL_3,apellido);
+        contentValues.put(COL_4,recinto);
+        contentValues.put(COL_5,anio);
+        long resultado =db.insert(TABLA_NOMBRE,null,contentValues);
+
+        if(resultado == -1)
+            return false;
+        else
+            return true;
+
+    }
+    public Cursor MostarTodos(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery(String.format("select * from %s",TABLA_NOMBRE),null);
+        return  res;
+    }
+
+
+    public Cursor bucarporid(String id) throws SQLException {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+
+
+        // TODO Auto-generated method stub
+        String[] columns = new String[]{COL_1,COL_2,COL_3,COL_4,COL_5};
+        Cursor c = db.query(TABLA_NOMBRE, columns, COL_1 + "="+ id, null, null, null, null);// change this line in this method
+        if(c!=null)
+        {
+            return c;
+        }
+        return null;
+    }
+
+    public Integer eliminarRegistro(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLA_NOMBRE,"id = ?",new String[]{id});
+    }
+
+    public boolean modificar(String id, String nombre, String apellido, String recinto, String anio){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_2,nombre);
+        contentValues.put(COL_3,apellido);
+        contentValues.put(COL_4,recinto);
+        contentValues.put(COL_5,anio);
+        db.update(TABLA_NOMBRE,contentValues,"id = ?",new String[]{id});
+
+
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/facci/votantesgz/MainActivityGZ.java
@@ -0,0 +1,121 @@
+package com.facci.votantesgz;
+
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivityGZ extends AppCompatActivity {
+    EditText txtNOMBRE,txtAPELLIDO,txtRECINTO,txtID,txtANIO,buscarporid;
+    DBHelper dbhelperSqlite;
+    Button ingresar,mostrartodo,modificar,eliminar;
+    public void mostrarMensaje(String titulo, String Mensaje){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(titulo);
+        builder.setMessage(Mensaje);
+        builder.show();
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_gz);
+        dbhelperSqlite = new DBHelper(this);
+        ingresar = (Button) findViewById(R.id.btnIngresar);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtNOMBRE = (EditText) findViewById(R.id.txtNOMBRE);
+                txtID = (EditText) findViewById(R.id.txtID);
+                txtAPELLIDO = (EditText) findViewById(R.id.txtAPELLIDO);
+                txtRECINTO = (EditText) findViewById(R.id.txtRECINTO);
+                txtANIO = (EditText) findViewById(R.id.txtANIO);
+                boolean estado = dbhelperSqlite.IngresarDatos(txtNOMBRE.getText().toString(), txtAPELLIDO.getText().toString(), txtRECINTO.getText().toString(), txtANIO.getText().toString());
+                if (estado == true) {
+                    Toast.makeText(MainActivityGZ.this, "Guardado Correctamente", Toast.LENGTH_SHORT).show();
+                    txtID.setText("");
+                    txtAPELLIDO.setText("");
+                    txtNOMBRE.setText("");
+                    txtANIO.setText("");
+                    txtRECINTO.setText("");
+                } else {
+                    Toast.makeText(MainActivityGZ.this, "OOOhhh ocurrio algo malo", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+        mostrartodo = (Button) findViewById(R.id.btnMostrar);
+        mostrartodo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Cursor res = dbhelperSqlite.MostarTodos();
+                if (res.getCount() == 0) {
+                    Toast.makeText(MainActivityGZ.this, "OOOhhh ocurrio algo malo", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+
+                while (res.moveToNext()) {
+                    buffer.append("Id : " + res.getString(0) + "\n");
+                    buffer.append("Nombre : " + res.getString(1) + "\n");
+                    buffer.append("Apellido : " + res.getString(2) + "\n");
+                    buffer.append("Recinto : " + res.getString(3) + "\n");
+                    buffer.append("Año nacimiento : " + res.getInt(4) + "\n\n");
+                }
+
+                Toast.makeText(MainActivityGZ.this, "XD", Toast.LENGTH_SHORT).show();
+                mostrarMensaje("Registros", buffer.toString());
+
+            }
+        });
+        buscarporid = (EditText) findViewById(R.id.txtID);
+        buscarporid.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                return false;
+            }
+        });
+
+        eliminar= (Button) findViewById(R.id.btnEliminar);
+        eliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtID = (EditText) findViewById(R.id.txtID);
+                Integer registrosEliminados = dbhelperSqlite.eliminarRegistro(txtID.getText().toString());
+                if(registrosEliminados > 0 ){
+                    Toast.makeText(MainActivityGZ.this,"Registro(s) Eliminado(s)",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivityGZ.this,"OOOhhh ocurrio algo malo no se elimino",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        modificar= (Button) findViewById(R.id.btnModificar);
+        modificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtNOMBRE = (EditText) findViewById(R.id.txtNOMBRE);
+                txtAPELLIDO= (EditText) findViewById(R.id.txtAPELLIDO);
+                txtRECINTO = (EditText) findViewById(R.id.txtRECINTO);
+                txtANIO = (EditText) findViewById(R.id.txtANIO);
+                txtID = (EditText) findViewById(R.id.txtID);
+
+                boolean Actualizador = dbhelperSqlite.modificar(String.valueOf(txtID.getText()),txtNOMBRE.getText().toString(),txtAPELLIDO.getText().toString(),txtRECINTO.getText().toString(),txtANIO.getText().toString());
+
+                if (Actualizador == true){
+                    Toast.makeText(MainActivityGZ.this,"Actualizado",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivityGZ.this,"OOOhhh ocurrio algo malo no se Actualizo",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }}
